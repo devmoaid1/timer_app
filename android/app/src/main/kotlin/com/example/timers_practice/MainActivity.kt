00c0897f39a83efe2602d5bdb14e223b5bd4fc86
devmoaid1 <moaidmohamed1312@gmail.com>
2024-12-06 16:04:39 +0200

    intial commit

---
+++ android/app/src/main/kotlin/com/example/timers_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.timers_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:timers_practice/timer_app.dart';
+
+void main() {
+  runApp(const TimerApp());
+}
