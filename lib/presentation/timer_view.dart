2052bdc4e116651110de9981fa6056d530ca6ec2
devmoaid1 <moaidmohamed1312@gmail.com>
2024-12-06 16:11:47 +0200

    added font

--- lib/presentation/timer_view.dart
+++ lib/presentation/timer_view.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/timer_view_body.dart';
+
 class TimerView extends StatelessWidget {
   const TimerView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Timer Screen'),
+        centerTitle: true,
+        elevation: 1.3,
+      ),
+      body: const TimerViewBody(),
+    );
   }
 }
